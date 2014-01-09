e03318ed665ccdd314e7784f5a22e21d35eaf3cf
Adam Rezich <adam@rezich.com>
2014-01-09 10:25:37 -0800

    added alpha engine and gss folder

---
+++ src/gss/main.c
@@ -0,0 +1,10 @@
+/* All content (C) 2013-2014 DigiPen (USA) Corporation, all rights reserved. */
+
+#include "../alpha/AEEngine.h"
+
+#pragma comment (lib, "../alpha/Alpha_Engine.lib")
+
+int gGameRunning = 1;
+int WINAPI WinMain(HINSTANCE instanceH, HINSTANCE prevInstanceH, LPSTR commandLine, int show) {
+
+}
