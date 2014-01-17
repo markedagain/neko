675922515cca135aa66f35edabd14fdf34cf0555
Adam Rezich <adam@rezich.com>
2014-01-16 20:07:44 -0800

    fixed errors from last hasty commit

--- NekoEngine/dictionary.c
+++ NekoEngine/dictionary.c
@@ -15,16 +15,19 @@ void sdict_set(DICT *dict, char *key, void *value) {
   if (vector_size(&dict->empties) > 0) {
     // TODO: HANDLE EMPTIES
   }
-  vector_append(&dict->keys, &key);
+  vector_append(&dict->keys, key);
   vector_append(&dict->values, value);
 }
 void *sdict_get(DICT *dict, char *key) {
-  int i;
+  unsigned int i;
   size_t keySize;
+  keySize = vector_size(&dict->keys);
 
   bool found = false;
-  for (i = 0; i < vector_size(&dict->keys); ++i) {
-    if (
+  for (i = 0; i < keySize; ++i) {
+    if (strcmp((char *)vector_get(&dict->keys, i), key)) {
+      return vector_get(&dict->values, i);
+    }
   }
   return NULL;
 }
