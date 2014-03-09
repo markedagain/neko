8d9908e1b9a4295704b1f966fa29eb131c506d9d
Eduardo Gorinstein <eduardogor64@gmail.com>
2014-03-09 16:33:25 -0700

    push for DH

--- Game School Simulator 2015/newsfeedlogic.c
+++ Game School Simulator 2015/newsfeedlogic.c
@@ -3,6 +3,7 @@
 #include "newsfeedlogic.h"
 #include "../NekoEngine/generictext.h"
 #include "../Nekoengine/spritetext.h"
+#include "easing.h"
 
 char *pushStrings[STRINGS_LAST] = {
   "Welcome to: %s!",
@@ -25,6 +26,18 @@ void comp_newsfeedlogic_initialize(COMPONENT *self, void *event) {
     pos.y += 10;
     comData->lines[i] = genericText_create(uiSpace, &pos, "textSprite", "fonts/gothic/12", textBuffer, &color, TEXTALIGN_LEFT, TEXTALIGN_MIDDLE);
   }
+
+  comData->fadeOutStartTime = (float)self->owner->space->game->systems.time.elapsed;
+}
+
+void comp_newsfeedlogic_logicUpdate(COMPONENT *self, void *event) {
+  int i;
+  CDATA_NEWSFEEDLOGIC *comData = (CDATA_NEWSFEEDLOGIC *)self->data;
+
+  for(i = 0; i < 5; i++) {
+    CDATA_SPRITETEXT *spriteText = (CDATA_SPRITETEXT *) entity_getComponentData(comData->lines[i], COMP_SPRITETEXT);
+    spriteText->color.a = e_quad_in((float)comData->fadeOutStartTime - (float)self->owner->space->game->systems.time.elapsed, 1.0f, -1.0f, 1.0f);
+  }
 }
 
 void comp_newsfeedlogic_push(COMPONENT *ptr, char *string) {
@@ -34,7 +47,7 @@ void comp_newsfeedlogic_push(COMPONENT *ptr, char *string) {
   ENTITY *newsFeed = space_getEntity(uiSpace, "newsFeed");
   CDATA_NEWSFEEDLOGIC *comData = (CDATA_NEWSFEEDLOGIC *)entity_getComponentData(newsFeed, COMP_NEWSFEEDLOGIC);
 
-  // Place the message in its sprite
+  
   if(comData->messages->count < 5) {
     int i;
 
@@ -42,6 +55,7 @@ void comp_newsfeedlogic_push(COMPONENT *ptr, char *string) {
       for(i = 0; i < comData->messages->count; i++) {
         CDATA_SPRITETEXT *spriteText = (CDATA_SPRITETEXT *) entity_getComponentData(comData->lines[comData->messages->count - i - 1], COMP_SPRITETEXT);
         genericText_setText(comData->lines[comData->messages->count - i], spriteText->text);
+        spriteText->color.a = 1;
       }
     }
   }
@@ -50,12 +64,20 @@ void comp_newsfeedlogic_push(COMPONENT *ptr, char *string) {
     for(i = 0; i < 4; i++) {
       CDATA_SPRITETEXT *spriteText = (CDATA_SPRITETEXT *) entity_getComponentData(comData->lines[4 - i - 1], COMP_SPRITETEXT);
       genericText_setText(comData->lines[4 - i], spriteText->text);
+      spriteText->color.a = 1;
     }
   }
 
+  // Place the message in its sprite
   sprintf(textBuffer, string);
   genericText_setText(comData->lines[0], textBuffer);
   list_insert_end(comData->messages, textBuffer);
+  {
+    CDATA_SPRITETEXT *spriteText = (CDATA_SPRITETEXT *) entity_getComponentData(comData->lines[0], COMP_SPRITETEXT);
+    spriteText->color.a = 1;
+  }
+
+  comData->fadeOutStartTime = (float)ptr->owner->space->game->systems.time.elapsed;
 }
 
 void comp_newsfeedlogic_destroy(COMPONENT *self, void *event){
@@ -69,4 +91,5 @@ void comp_newsfeedlogic(COMPONENT *self) {
 
   COMPONENT_INIT(self, COMP_NEWSFEEDLOGIC, data);
   self->events.initialize = comp_newsfeedlogic_initialize;
+  self->events.logicUpdate = comp_newsfeedlogic_logicUpdate;
 }
