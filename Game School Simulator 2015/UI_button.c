2794d46b96a960755493265477ce85e9970112ae
Eduardo Gorinstein <eduardogor64@gmail.com>
2014-03-29 18:25:16 -0700

    Added hover text to upgrade

--- Game School Simulator 2015/UI_button.c
+++ Game School Simulator 2015/UI_button.c
@@ -55,7 +55,11 @@ void comp_UI_buttonUpdate(COMPONENT *self, void *event) {
       VEC3 position;
       VEC4 color;
       if(transform->translation.x <= -249)
+      {
         vec3_set(&position, transform->translation.x + 40, transform->translation.y + 30, 0);
+        if(transform->translation.y >= 0)
+          vec3_set(&position, transform->translation.x, transform->translation.y - 60, 0);
+      }
       else
         vec3_set(&position, transform->translation.x, transform->translation.y + 30, 0);
       vec4_set(&color, 0, 0, 0, 1 );
@@ -126,6 +130,11 @@ void comp_UI_buttonUpdate(COMPONENT *self, void *event) {
 
       case BUTTON_BUILDFIGURE:
         sprintf(buffer, "Art +++\n$150,000");
+        genericText_setText(data->roomInfoUI, buffer);
+        break;
+
+      case BUTTON_ROOM_UPGRADE:
+        sprintf(buffer, "Upgrade!\n$5,000");
         genericText_setText(data->roomInfoUI, buffer);
         break;
 
